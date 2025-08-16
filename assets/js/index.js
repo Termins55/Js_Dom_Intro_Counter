da9901d7e5e8479c5bbac430befb1d03f534e130
Termins55 <kevgame123@gmail.com>
2025-08-16 15:21:15 +0300

    Refactor counter with MVVM

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,37 @@
 // отримати посилання кнопок
 
 // View - Spagetti
-
+//      | |
+//      \ /
 // View - *** - Model
+
+// Model - дані + методи роботи з ним
+class Counter {
+  constructor(counter) {
+    this.counter = counter;
+  }
+  dec() {
+    this.counter--;
+  }
+  inc() {
+    this.counter++;
+  }
+}
+
+// ViewModel
 
 const [decBtn, incBtn] = document.querySelectorAll(".counterBtn");
 
 const counterEl = document.querySelector("#counter");
 
-// Model
-
-let counter = 0;
-counterEl.textContent = counter;
-
+const count = new Counter(0);
+updateView();
 // навісити обробник на кнопнку -
 // в обробнику зменьшити лічильник
 
 function decCount(e) {
-  counter--;
-  counterEl.textContent = counter;
+  count.dec();
+  updateView();
 }
 
 decBtn.addEventListener("click", decCount);
@@ -26,8 +39,12 @@ decBtn.addEventListener("click", decCount);
 // реалізувати натискання на +
 
 function incCount(e) {
-  counter++;
-  counterEl.textContent = counter;
+  count.inc();
+  updateView();
 }
 
 incBtn.addEventListener("click", incCount);
+
+function updateView() {
+  counterEl.textContent = count.counter;
+}
